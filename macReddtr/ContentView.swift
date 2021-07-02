8cc0b37ba960fe9f2c2873dcb05be66ed827bc6d
Brata Saputra <my.developer@outlook.com>
2021-07-02 23:29:28 +0700

    Initial commit

---
+++ macReddtr/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  macReddtr
+//
+//  Created by Brata on 02/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ macReddtr/macReddtrApp.swift
@@ -0,0 +1,17 @@
+//
+//  macReddtrApp.swift
+//  macReddtr
+//
+//  Created by Brata on 02/07/21.
+//
+
+import SwiftUI
+
+@main
+struct macReddtrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
